86d8eae77500ad2018ca5cfe3c7f23e7ce491401
niku <nikuliu@foxmail.com>
2023-07-29 00:40:30 +0800

    🦄 refactor: 测试下工具方法

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 
 pub mod dom;
+pub mod html_parser;
 
 fn main() {
-    println!("Hello, world!");
+    let html = r#"
+    <html>
+        <head>
+            <title>HTML Parser</title>
+        </head>
+        <body>
+            <h1>HTML Parser</h1>
+            <div id="main" class="test">
+                <p>Hello <em>world</em>!</p>
+                <p>Rust is <em>fun</em>!</p>
+            </div>
+        </body>
+    </html>
+    "#;
+
+    let mut parser = html_parser::Parser {
+        pos: 0,
+        input: html.to_string(),
+    };
+
+    for i in 0..10 {
+        println!("idx:{}, char: {}, pos:{}", i, parser.consume_char(), parser.pos);
+    }
 }
